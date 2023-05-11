b0f719b12f36750dcc2f985f8e08be3a79fc7610
alphaprime8 <alphaprime8888@gmail.com>
2023-05-11 18:06:31 +0800

    drafted series gen

--- src/lib.rs
+++ src/lib.rs
@@ -21,12 +21,18 @@ const START_PRICE_SOL: f64 = 50.;
 const MIN_VOLUME_SOL: f64 = 1.;
 const MAX_VOLUME_SOL: f64 = 100.;
 
+const MIN_FRAC_DELTA: f64 = 0.0001;
+const MAX_FRAC_DELTA: f64 = 0.1000;
+
+const MIN_WICK_FRAC: f64 = 0.1;
+const MAX_WICK_FRAC: f64 = 0.5;
+
 const SECOND_PER_MINUTE: usize = 60;
 const MINUTES_PER_DAY: usize = 1440;
 const DAYS_PER_MONTH: usize = 30;
-const PROB_REVERSAL: f64 = 0.5;
+const PROB_REVERSAL: f64 = 0.6;
 
-const DELTA_COEFFICIENT_SOL: f64 = 1.0;
+// const DELTA_COEFFICIENT_SOL: f64 = 1.0;
 
 const CANDLE_SIZE_SECONDS: u64 = 60;
 
@@ -46,23 +52,51 @@ pub fn generate_one_month_minute_candles(){
 
     for i in 0..num_candles {
 
-        // TODO generate new candle
-        last_time_begin += CANDLE_SIZE_SECONDS;
+        let frac_delta = utils::get_random_in_range(MIN_FRAC_DELTA, MAX_FRAC_DELTA);
+        let frac_wick = utils::get_random_in_range(MIN_WICK_FRAC, MAX_WICK_FRAC);
+        let delta = last_close * frac_delta;
+        let wick = delta * frac_wick;
 
 
+        let new_close;
+        let high;
+        let low;
+        if is_up {
+            new_close = last_close + delta;
+            high = new_close + wick;
+            low = last_close - wick;
+        } else {
+            new_close = last_close - delta;
+            high = last_close + wick;
+            low = new_close - wick;
+        }
 
+        if utils::get_random_in_range(0., 1.) < PROB_REVERSAL {
+            is_up = !is_up;
+        }
+
+        if new_close > MAX_PRICE_SOL {
+            is_up = false;
+        }
+        if new_close < MIN_PRICE_SOL {
+            is_up = true;
+        }
 
 
         let new_candle = Candle {
             time_begin: last_time_begin,
             time_end: last_time_begin + CANDLE_SIZE_SECONDS,
-            open: 0.0,
-            high: 0.0,
-            low: 0.0,
-            close: 0.0,
+            open: last_close,
+            high,
+            low,
+            close: new_close,
             volume: utils::get_random_in_range(MIN_VOLUME_SOL, MAX_VOLUME_SOL),
         };
         candles.push(new_candle);
+
+        // update close
+        last_time_begin += CANDLE_SIZE_SECONDS;
+        last_close = new_close;
     }
 
 
